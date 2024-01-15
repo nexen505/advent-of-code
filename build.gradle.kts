7e912a83db56d426fd21927753e4aad6c8adf3b0
Ilia Komarov <il.komarov@tinkoff.ru>
2024-01-15 16:21:17 +0400

    AoC 2022: implement Day 13

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     kotlin("jvm") version "1.9.21"
 }
 
+dependencies {
+    implementation(platform("com.fasterxml.jackson:jackson-bom:2.16.1"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+}
+
 tasks {
     wrapper {
         gradleVersion = "8.5"
