5c2a7999ec3ce22144f25887c7cef8bba0b02e5b
Ilia Komarov <nexen505@yandex.ru>
2024-11-24 10:28:46 +0100

    cosmetics

--- src/main/kotlin/IterableUtils.kt
+++ src/main/kotlin/IterableUtils.kt
@@ -2,23 +2,14 @@ import kotlin.math.abs
 import kotlin.math.hypot
 
 // trapezoid formula https://en.wikipedia.org/wiki/Shoelace_formula
-fun Iterable<Pair<Long, Long>>.calculateArea(): Double {
-    var area = 0.0
-    for ((prev, cur) in cycle()) {
-        area += (prev.second + cur.second) * (prev.first - cur.first) / 2.0
-    }
+fun Iterable<Pair<Long, Long>>.calculateArea(): Double = cycle()
+    .map { (prev, cur) -> (prev.second + cur.second) * (prev.first - cur.first) / 2.0 }
+    .sum()
+    .let { abs(it) }
 
-    return abs(area)
-}
-
-fun Iterable<Pair<Long, Long>>.calculatePerimeter(): Double {
-    var length = 0.0
-    for ((prev, cur) in cycle()) {
-        length += hypot(cur.first.toDouble() - prev.first, cur.second.toDouble() - prev.second)
-    }
-
-    return length
-}
+fun Iterable<Pair<Long, Long>>.calculatePerimeter(): Double = cycle()
+    .map { (prev, cur) -> hypot(cur.first.toDouble() - prev.first, cur.second.toDouble() - prev.second) }
+    .sum()
 
 // https://en.wikipedia.org/wiki/Pick%27s_theorem
 fun Iterable<Pair<Long, Long>>.calculateInterior(): Double = calculateArea() - calculatePerimeter() / 2 + 1
@@ -30,5 +21,3 @@ fun Iterable<Long>.mul(): Long = fold(1L) { a, b -> a * b }
 
 fun Iterable<Long>.lcm(): Long = fold(1L) { res, v -> res lcm v }
 fun Sequence<Long>.lcm(): Long = fold(1L) { res, v -> res lcm v }
-
-fun <T> List<T>.equalsIgnoreOrder(other: List<T>) = this.size == other.size && this.toSet() == other.toSet()
